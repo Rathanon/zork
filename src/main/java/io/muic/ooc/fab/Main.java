50ba483f634ea25de2c358801950111a484b120e
Rathanon <rathanon.sin@student.mahidol.edu>
2021-06-13 20:27:03 +0700

    Scanner

--- src/main/java/io/muic/ooc/fab/Main.java
+++ src/main/java/io/muic/ooc/fab/Main.java
@@ -1,7 +1,14 @@
 package io.muic.ooc.fab;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        while (true){
+            Scanner in = new Scanner(System.in);
+            String s = in.nextLine();
+            System.out.println(s);
+        }
 
     }
 }
